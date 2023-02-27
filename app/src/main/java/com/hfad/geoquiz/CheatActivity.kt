e927c35eaac4e740d03883994bfb12edaad8dcaf
Aleksey <meysonsib200189@mail.ru>
2023-02-27 15:33:10 +0700

    вторая активити

---
+++ app/src/main/java/com/hfad/geoquiz/CheatActivity.kt
@@ -0,0 +1,48 @@
+package com.hfad.geoquiz
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+private const val EXTRA_ANSWER_IS_TRUE = "extra_answer_is_true"
+
+private lateinit var answerTextView: TextView
+private lateinit var showAnswerButton: Button
+
+class CheatActivity : AppCompatActivity() {
+
+    private var answerIsTrue = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+
+        initView()
+        val answerText = when {
+            answerIsTrue -> R.string.true_button
+            else -> R.string.false_button
+        }
+        answerTextView.setText(answerText)
+    }
+
+   private fun initView(){
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/hfad/geoquiz/MainActivity.kt
+++ app/src/main/java/com/hfad/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hfad.geoquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,40 +9,52 @@ import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 
 private const val TAG = " MainActivity"
+private const val KEY_INDEX = "index"
+private const val KEY_SCORE = "score"
 
 class MainActivity : AppCompatActivity() {
 
-
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-    private var currentIndex = 0
-    private var scoreIndex = 0
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this)[QuizViewModel::class.java]
+    }
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var prevButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
     private lateinit var scoreTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            quizViewModel.currentIndex = 0
+            quizViewModel.scoreIndex = 0
+        } else {
+            quizViewModel.currentIndex =
+                savedInstanceState.getInt(KEY_INDEX, quizViewModel.currentIndex)
+            quizViewModel.scoreIndex =
+                savedInstanceState.getInt(KEY_SCORE, quizViewModel.scoreIndex)
+        }
 
         initView()
         selectAnswer()
-        checkQuestion()
+        checkQuestion()//след пред
         updateQuestion()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+        outState.putInt(KEY_SCORE, quizViewModel.scoreIndex)
     }
 
     private fun initView() {
@@ -49,47 +62,54 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         prevButton = findViewById(R.id.prev_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
         scoreTextView = findViewById(R.id.score_text_view)
     }
 
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
+    //ответил правильно или нет + начисление очков,переход к след вопросу с отрисовкой очков на экране
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
-        var messageResId = if (userAnswer == correctAnswer) {
-            score()
-            Toast.makeText(this, "Угадали! ваши очки - ${scoreIndex}", Toast.LENGTH_SHORT).show()
-                .let {
-                    currentIndex = (currentIndex + 1) % questionBank.size
-                    updateQuestion()
-                }
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+        if (userAnswer == correctAnswer) {
+            quizViewModel.score()
+            scoreTextView.text =
+                quizViewModel.scoreIndex.toString()//FIXME очки не сохраняются после отрисовки после поворота экрана
+            Toast.makeText(
+                this,
+                "Угадали! ваши очки - ${quizViewModel.scoreIndex}",
+                Toast.LENGTH_SHORT
+            ).show()
+            quizViewModel.moveToNext()
+            updateQuestion()
         } else {
-            Toast.makeText(this, "Ошиблись! ваши очки - ${scoreIndex}", Toast.LENGTH_SHORT).show()
-            currentIndex = (currentIndex + 1) % questionBank.size
+            Toast.makeText(
+                this,
+                "Ошиблись! ваши очки - ${quizViewModel.scoreIndex}",
+                Toast.LENGTH_SHORT
+            ).show()
+            quizViewModel.moveToNext()
             updateQuestion()
         }
     }
 
-    private fun score() {
-        scoreIndex = scoreIndex + 1
-        scoreTextView.setText(scoreIndex.toString())
-    }
-
+    //след или предыдущий вопрос
     private fun checkQuestion() {
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
         prevButton.setOnClickListener {
-            currentIndex = (currentIndex - 1) % questionBank.size
+            quizViewModel.moveToPrev()
             updateQuestion()
         }
     }
 
+    //выбор ответа пользователя
     private fun selectAnswer() {
         trueButton.setOnClickListener {
             checkAnswer(true)
@@ -98,6 +118,11 @@ class MainActivity : AppCompatActivity() {
         falseButton.setOnClickListener {
             checkAnswer(false)
         }
+        cheatButton.setOnClickListener {
+            // Начало CheatActivity
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivity(intent)
+        }
     }
 }
-//комит
